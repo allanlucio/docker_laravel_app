7a1d62b3e21896e358e44eee269be553e8597ed4
Allan Lucio <luciocorreia.allan@gmail.com>
2019-06-23 10:51:41 -0300

    Fixed error on entrypoint

--- .docker/app/entrypoint.sh
+++ .docker/app/entrypoint.sh
@@ -5,5 +5,5 @@ composer install
 php artisan key:generate
 dockerize -wait tcp://db:3306 -timeout 40s 
 php artisan migrate
-RUN chown -R www-data:www-data /var/www
+chown -R www-data:www-data /var/www
 php-fpm
